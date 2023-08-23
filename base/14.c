c4c63d83ce565c3b293987f821263afa468f82cb
gantrol <gantrol.h@qq.com>
2023-08-24 02:03:45 +0800

    ex14 extra credit

--- base/14.c
+++ base/14.c
@@ -1,30 +1,31 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+#include <wctype.h>
 
 // forward declarations
-int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char arg[], int length);
 
 void print_arguments(int argc, char *argv[]) {
   int i = 0;
 
   for (i = 0; i < argc; i++) {
-    print_letters(argv[i]);
+    char *arg = argv[i];
+    print_letters(arg, strlen(arg));
   }
 }
 
-void print_letters(char arg[]) {
+void print_letters(char arg[], int length) {
   int i = 0;
-  for (i = 0; arg[i] != '\0'; i++) {
+  for (i = 0; i < length; i++) {
     char ch = arg[i];
-    if (can_print_it(ch)) {
+    if (isalnum((int)ch) || iswblank((int)ch)) {
+      // if (isalpha((int)ch) || isblank((int)ch)) {
       printf("'%c' == '%d'", ch, ch);
     }
   }
   printf("\n");
 }
-
-int can_print_it(char ch) { return isalpha((int)ch) || isblank((int)ch); }
 
 int main(int argc, char *argv[]) {
   print_arguments(argc, argv);
