8e4a326131f2bc67715953eda5ced815cc4ba34e
gantrol <gantrol.h@qq.com>
2023-08-25 10:49:40 +0800

    Refactor code formatting for better readability
    Code formatting has been tweaked in several functions in base/14.c. The opening braces of the functions were moved to a new line to clearly show the beginning and the end of a function scope. This change was made to improve code readability and maintainability by adhering to a consistent coding style throughout the project.

--- base/14.c
+++ base/14.c
@@ -6,28 +6,31 @@
 // forward declarations
 void print_letters(char arg[], int length);
 
-void print_arguments(int argc, char *argv[]) {
-  int i = 0;
+void print_arguments(int argc, char *argv[])
+{
+    int i = 0;
 
-  for (i = 0; i < argc; i++) {
-    char *arg = argv[i];
-    print_letters(arg, strlen(arg));
-  }
-}
-
-void print_letters(char arg[], int length) {
-  int i = 0;
-  for (i = 0; i < length; i++) {
-    char ch = arg[i];
-    if (isalnum((int)ch) || iswblank((int)ch)) {
-      // if (isalpha((int)ch) || isblank((int)ch)) {
-      printf("'%c' == '%d'", ch, ch);
+    for (i = 0; i < argc; i++) {
+        char *arg = argv[i];
+        print_letters(arg, strlen(arg));
     }
-  }
-  printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-  print_arguments(argc, argv);
-  return 0;
+void print_letters(char arg[], int length)
+{
+    int i = 0;
+    for (i = 0; i < length; i++) {
+        char ch = arg[i];
+        if (isalnum((int) ch) || iswblank((int) ch)) {
+            // if (isalpha((int)ch) || isblank((int)ch)) {
+            printf("'%c' == '%d'", ch, ch);
+        }
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+    print_arguments(argc, argv);
+    return 0;
 }
