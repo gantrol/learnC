51706bfd01ec3fbe19d74fa119ac1e886af7e03d
gantrol <gantrol.h@qq.com>
2023-08-25 15:35:42 +0800

    ex15: revert to init

--- base/15.c
+++ base/15.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     // create two arrays we care about
-    int count = 5;
     int ages[] = {31, 29, 25, 28, 28};
     char *names[] = {
             "蠢狗君", "大明",
@@ -12,6 +10,7 @@ int main(int argc, char *argv[])
     };
 
     // safely get the size of ages
+    int count = sizeof(ages) / sizeof(int); // TODO: why?
     int i;
 
     // first way using indexing
@@ -21,26 +20,29 @@ int main(int argc, char *argv[])
 
     printf("---\n");
 
+    // setup the pointers to the start of the arrays
+    int *cur_age = ages;
+    char **cur_name = names; // TODO: why not []?
+
     // second way using pointers
     for (i = 0; i < count; i++) {
         printf("%s活了 %d 年。\n",
-                names[i], ages[i]);
+               *(cur_name + i), *(cur_age + i));
     }
 
     printf("---\n");
 
     // third way, pointers are just arrays
     for (i = 0; i < count; i++) {
-        printf("%s is %d years old again.\n", names[i], ages[i]);
+        printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
     }
 
     printf("---\n");
 
     // fourth way with pointers in a stupid complex way
-    // try rewriting the loops so that they start at the end of the arrays and go to the beginning.
-    for (i = count - 1;
-         i >= 0; i--) {
-        printf("%s lived %d years so far.\n", names[i], ages[i]);
+    for (cur_name = names, cur_age = ages;
+         (cur_age - ages) < count; cur_name++, cur_age++) {
+        printf("%s lived %d years so far.\n", *cur_name, *cur_age);
     }
 
     return 0;
